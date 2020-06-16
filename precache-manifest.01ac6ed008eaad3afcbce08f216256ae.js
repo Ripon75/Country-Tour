89152bd0b8cdccb11da2f5d844c7af47ae3a0ae2
riponahmed <riponahmedjrtr@gmail.com>
2020-06-16 20:34:35 +0600

    Updates

---
+++ precache-manifest.01ac6ed008eaad3afcbce08f216256ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a3b5a9f403e7a3bd9013113ac8ea39",
+    "url": "/Country-Tour/index.html"
+  },
+  {
+    "revision": "16de5111d450f773f10d",
+    "url": "/Country-Tour/static/css/main.0f6b8a01.chunk.css"
+  },
+  {
+    "revision": "b2317ace93c01c4de9ea",
+    "url": "/Country-Tour/static/js/2.0e9ccaee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Country-Tour/static/js/2.0e9ccaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16de5111d450f773f10d",
+    "url": "/Country-Tour/static/js/main.e2d96b52.chunk.js"
+  },
+  {
+    "revision": "d109937dc0738913f277",
+    "url": "/Country-Tour/static/js/runtime-main.29e77901.js"
+  },
+  {
+    "revision": "4dc3d231ce299c7a5d98133b3708f152",
+    "url": "/Country-Tour/static/media/Ripon_Ahmed.4dc3d231.jpg"
+  }
+]);
